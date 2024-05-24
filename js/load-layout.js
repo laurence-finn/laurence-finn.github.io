21b6bb2427df1b42014e264336603d564bd46ab0
Laurence Finn <laurence.c.finn@gmail.com>
2024-05-24 08:16:37 -0500

    Move the _header file to the root directory, and edit the language of the website.

--- js/load-layout.js
+++ js/load-layout.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Fetch is a built-in function that makes an HTTP request to the server for the header file.
     fetch('/_header.html') 
       // Response.text is returned as a "promise" which is a way to handle asynchronous code.
+      // Each "then" function is a callback that occurs when the previous function is complete.
       .then(response => response.text()) 
-        //data => is a lambda function that takes the data returned from the server 
-        //and inserts it into the header element.
+      // Take the data from the response and insert it into the header element.
       .then(data => {
         document.querySelector('header').innerHTML = data; 
 
