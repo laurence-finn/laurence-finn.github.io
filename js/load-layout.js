a77ac14d5465b73ef1b1178a10327a3d52324801
Laurence Finn <laurence.c.finn@gmail.com>
2024-05-23 23:18:39 -0500

    Add an About page with a draft autobiography.

--- js/load-layout.js
+++ js/load-layout.js
@@ -1,7 +1,14 @@
+// File: load-layout.js
+// Description: Load the reusable header from the server using the fetch API and insert the text into the header element.
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('/_header.html')
-      .then(response => response.text())
+    // Fetch is a built-in function that makes an HTTP request to the server for the header file.
+    fetch('/_header.html') 
+      // Response.text is returned as a "promise" which is a way to handle asynchronous code.
+      .then(response => response.text()) 
+        //data => is a lambda function that takes the data returned from the server 
+        //and inserts it into the header element.
       .then(data => {
-        document.querySelector('header').innerHTML = data;
+        document.querySelector('header').innerHTML = data; 
+
       });
   });
